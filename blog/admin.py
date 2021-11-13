1928436723a2037be852829f9679ad1b26ae5d1b
viktor <vitek@goykt.ru>
2021-11-13 12:55:24 +0900

    add fk post to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 
-from blog.models import Post, PostTag
+from blog.models import Post, PostTag, ForeignPost
+
+
+class ForeignPostInline(admin.TabularInline):
+    model = ForeignPost
+    fk_name = 'parent'
 
 
 @admin.register(PostTag)
@@ -13,3 +18,4 @@ class PostTagAdmin(admin.ModelAdmin):
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = '__all__'
     filter_horizontal = ('tags',)
+    inlines = [ForeignPostInline]
